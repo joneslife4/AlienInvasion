eec65b6c9dfff1448f21f5dec1ed31c8c94edd58
Marquis Jones <mjones@Marquiss-MacBook-Air.local>
2024-06-16 22:30:33 -0500

    Initial commit

---
+++ settings.py
@@ -0,0 +1,55 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's static settings"""
+
+        # Alien settings
+        self.alien_speed = 1.5
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right: -1 represents left
+        self.fleet_direction = 1
+
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (25, 25, 80)
+
+        # Ship settings
+        self.ship_speed = 2
+        self.ship_limit = 3
+
+        # Bullet settings
+        self.bullet_speed = 1.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (250, 110, 155)
+        self.bullets_allowed = 5
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.5
+
+        # How quickly the alien point value increases
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize the settings the game throughout the game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+        # Scoring
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings and alien point values"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
